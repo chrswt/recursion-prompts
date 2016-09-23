2c02be23a24c706f1b9cda6ec1e8eaaf1b897690
Christopher Tham <chriswt91@gmail.com>
2016-09-22 18:49:03 -0700

    Complete isEven

--- src/recursion.js
+++ src/recursion.js
@@ -28,6 +28,8 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  return n >= 0 && n <= 1 ? n === 0 :
+  n < 0 ? isEven(n + 2) : isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
